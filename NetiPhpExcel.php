bcc3d5992d7f693b7fea3f12c2f7bc857b6c43a2
Sascha Brüggenolte <sb@netinventors.de>
2016-10-27 11:00:03 +0200

    prevent installation, if vendor is missing

--- NetiPhpExcel.php
+++ NetiPhpExcel.php
@@ -8,7 +8,20 @@
 namespace NetiPhpExcel;
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
 
 class NetiPhpExcel extends Plugin
 {
+    /**
+     * @param InstallContext $context
+     * @throws \Exception
+     */
+    public function install(InstallContext $context)
+    {
+        if (! is_readable(__DIR__ . DIRECTORY_SEPARATOR . 'vendor')) {
+            throw new \Exception('Please run "composer install" before you install the plugin!');
+        }
+
+        parent::install($context);
+    }
 }
